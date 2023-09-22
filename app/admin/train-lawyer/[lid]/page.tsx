481c6f14ea6f71c0fc91599e718f7689a41ba37d
gaonkarBhai <naveengaonkar014@gmail.com>
2023-09-22 21:22:03 +0530

    interface,train-lawyer

---
+++ app/admin/train-lawyer/[lid]/page.tsx
@@ -0,0 +1,27 @@
+import prismadb from "@/lib/prismadb";
+import LawyerForm from "./components/lawyerForm";
+
+interface lawyerIdProps{
+    params:{
+        lid:string
+    }
+}
+
+
+export default async function LawyerPage({params}:lawyerIdProps){
+    console.log(params.lid);
+    const lawyer = await prismadb.lawyer.findUnique({
+        where:{
+            id: params.lid
+        },
+    })
+    const category = await prismadb.category.findMany()
+    return(
+        <LawyerForm 
+        initialData={lawyer}
+        categories={category}
+        />
+
+    )
+
+}
