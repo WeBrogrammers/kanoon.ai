f1df179a411c63907cf2ccb1108c83bbabb56d2d
gaonkarBhai <naveengaonkar014@gmail.com>
2023-09-22 12:33:10 +0530

    ಇನಷಯಲ ಕಮಟ

---
+++ app/chat/components/uploadfile.tsx
@@ -0,0 +1,27 @@
+import { Inbox, Loader2 } from "lucide-react";
+
+export default function Uploadfile() {
+    return (
+    //   <div className="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg">
+    //     <div className="h-20 w-20 rounded-full border overflow-hidden">
+    //       <img src="https://avatars3.githubusercontent.com/u/2763884?s=128" alt="Avatar" className="h-full w-full" />
+    //     </div>
+    //     <div className="text-sm font-semibold mt-2">Aminos Co.</div>
+    //     <div className="text-xs text-gray-500">Lead UI/UX Designer</div>
+    //     <div className="flex flex-row items-center mt-3">
+    //       <div className="flex flex-col justify-center h-4 w-8 bg-indigo-500 rounded-full">
+    //         <div className="h-3 w-3 bg-white rounded-full self-end mr-1" />
+    //       </div>
+    //       <div className="leading-none ml-1 text-xs">Active</div>
+    //     </div>
+    //   </div>
+      <div className="p-2 bg-white rounded-xl">
+      <div className=
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col"
+      >
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop Your File Here</p>
+      </div>
+    </div>
+    )
+}
