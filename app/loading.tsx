def11b82391f0e0f6946981abd7c9cf196fdf4be
gaonkarBhai <naveengaonkar014@gmail.com>
2023-09-25 22:14:11 +0530

    home, 404, loader

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <div className="loader-div">
+            <span className="loader">
+                <span />
+                <span />
+            </span>
+        </div>
+
+    )
+}
